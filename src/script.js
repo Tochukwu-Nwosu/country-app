532f477e1ee30c06b78bb616cdf9bb71bbbc19c8
User-XYZ <nwosu4686@gmail.com>
2022-12-23 23:04:44 +0100

    created country information

--- src/script.js
+++ src/script.js
@@ -10,21 +10,6 @@ button.addEventListener('click', () => {
         .then(response => response.json())
         .then(country => {
             country = country[0];
-            // console.log(country);
-            // console.log('name:', country.name.common);
-            // console.log(country.capital.join(', '));
-            // console.log(country.flags.svg);
-            // console.log(country.coatOfArms.svg)
-            // console.log(country.region);
-            // console.log(country.subregion);
-            // console.log(country.population);
-            // console.log(Object.values(country.currencies)[0]['name'], Object.values(country.currencies)[0]['symbol']);
-            // console.log(Object.values(country.languages).join(', '));
-            // console.log(country.timezones.join(', '));
-            // console.log(country.idd.root + '' + country.idd.suffixes[0]);
-            // console.log(country.tld[0]);
-            // console.log(country.car.side, country.car.signs[0]);
-            // console.log(country.maps.googleMaps)
 
             result.innerHTML = `
                 <div class="flex flex-col justify-center items-center py-2 my-4">
@@ -34,6 +19,50 @@ button.addEventListener('click', () => {
                     </div>
                     <h3 class="mb-4 font-semibold">${country.name.common.toUpperCase()}</h3>
                     <button class="px-7 py-3 rounded-3xl bg-indigo-600 text-gray-100 text-sm font-semibold tracking-wider hover:bg-indigo-400 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-opacity-50 transition ease-linear" id="toggle-btn">Coat Of Arm</button>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Capital:</h4>
+                    <span class="col-span-3">${country.capital.join(', ')}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Countinent:</h4>
+                    <span class="col-span-3 ml-4">${country.region}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Subregion:</h4>
+                    <span class="col-span-3 ml-2">${country.subregion}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Population:</h4>
+                    <span class="col-span-3 ml-2">${country.population}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Currency:</h4>
+                    <span class="col-span-3">${Object.values(country.currencies)[0]['name']} ${Object.values(country.currencies)[0]['symbol']}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Language:</h4>
+                    <span class="col-span-3">${Object.values(country.languages).join(', ')}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Time Zone:</h4>
+                    <span class="col-span-3">${country.timezones.join(', ')}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Dial Code:</h4>
+                    <span class="col-span-3">${country.idd.root}${country.idd.suffixes[0]}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">TLD:</h4>
+                    <span class="col-span-3">${country.tld[0]}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Traffic:</h4>
+                    <span class="col-span-3">${country.car.side} | ${country.car.signs[0]}<span>
+                </div>
+                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
+                    <h4 class="font-semibold">Map:</h4>
+                    <span class="col-span-3"><a class="text-indigo-500 hover:text-indigo-400 rounded-lg outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-opacity-50 transition ease-linear" href="${country.maps.googleMaps}">Map Of ${country.name.common}<span>
                 </div>
             `
             let toggleButton = document.querySelector('#toggle-btn');
