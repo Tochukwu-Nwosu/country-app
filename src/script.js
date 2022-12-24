225341c2b6ea4f7cc871e87cc8bdee17baef5e87
User-XYZ <nwosu4686@gmail.com>
2022-12-24 03:36:13 +0100

    made application responsive

--- src/script.js
+++ src/script.js
@@ -11,74 +11,84 @@ button.addEventListener('click', () => {
         .then(country => {
             country = country[0];
 
+            const showDialcode = () => {
+                if(country.idd.suffixes.length > 1) {
+                    return country.idd.root;
+                }else {
+                    return `${country.idd.root}${country.idd.suffixes[0]}`;
+                }
+            }
+            
+
             result.innerHTML = `
                 <div class="flex flex-col justify-center items-center py-2 my-4">
                     <div class="mb-4">
                         <img class="w-60 shadow-md" src="${country.flags.svg}" id="flag" alt="${country.name.common.toLowerCase()} flag">
-                        <img class="hidden w-60 shadow-md" id="coat-of-arm" src="${country.coatOfArms.svg}" alt="${country.name.common.toLowerCase()} coat of arm">
+                        <img class="hidden w-60 h-60 object-contain shadow-md" id="coat-of-arms" src="${country.coatOfArms.svg}" alt="${country.name.common.toLowerCase()} coat of arms">
                     </div>
-                    <h3 class="mb-4 font-semibold">${country.name.common.toUpperCase()}</h3>
-                    <button class="px-7 py-3 rounded-3xl bg-indigo-600 text-gray-100 text-sm font-semibold tracking-wider hover:bg-indigo-400 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-opacity-50 transition ease-linear" id="toggle-btn">Coat Of Arm</button>
+                    <h3 class="mb-4 text-lg sm:text-xl font-semibold">${country.name.common.toUpperCase()}</h3>
+                    <button class="px-5 sm:px-7 py-2 sm:py-3 rounded-3xl bg-indigo-600 text-gray-100 text-sm font-semibold tracking-wider hover:bg-indigo-400 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-opacity-50 transition ease-linear" id="toggle-btn">Coat Of Arms</button>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Capital:</h4>
-                    <span class="col-span-3">${country.capital.join(', ')}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Capital:</h4>
+                    <span>${country.capital.join(', ')}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Countinent:</h4>
-                    <span class="col-span-3 ml-4">${country.region}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Countinent:</h4>
+                    <span>${country.region}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Subregion:</h4>
-                    <span class="col-span-3 ml-2">${country.subregion}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Subregion:</h4>
+                    <span>${country.subregion}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Population:</h4>
-                    <span class="col-span-3 ml-2">${country.population}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Population:</h4>
+                    <span>${country.population}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Currency:</h4>
-                    <span class="col-span-3">${Object.values(country.currencies)[0]['name']} ${Object.values(country.currencies)[0]['symbol']}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Currency:</h4>
+                    <span>${Object.values(country.currencies)[0]['name']} ${Object.values(country.currencies)[0]['symbol']}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Language:</h4>
-                    <span class="col-span-3">${Object.values(country.languages).join(', ')}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Language:</h4>
+                    <span>${Object.values(country.languages).join(', ')}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Time Zone:</h4>
-                    <span class="col-span-3">${country.timezones.join(', ')}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Time Zone:</h4>
+                    <span>${country.timezones.join(', ')}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Dial Code:</h4>
-                    <span class="col-span-3">${country.idd.root}${country.idd.suffixes[0]}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Dial Code:</h4>
+                    <span>${showDialcode()}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">TLD:</h4>
-                    <span class="col-span-3">${country.tld[0]}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">TLD:</h4>
+                    <span>${country.tld[0]}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Traffic:</h4>
-                    <span class="col-span-3">${country.car.side} | ${country.car.signs[0]}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Traffic:</h4>
+                    <span>${country.car.side} | ${country.car.signs[0]}<span>
                 </div>
-                <div class="grid grid-cols-4 border-b-2 border-gray-300 py-2">
-                    <h4 class="font-semibold">Map:</h4>
-                    <span class="col-span-3"><a class="text-indigo-500 hover:text-indigo-400 rounded-lg outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-opacity-50 transition ease-linear" href="${country.maps.googleMaps}">Map Of ${country.name.common}<span>
+                <div class="border-b-2 border-gray-300 py-2 sm:text-lg">
+                    <h4 class="inline-block mr-2 font-semibold">Map:</h4>
+                    <span><a class="text-indigo-500 hover:text-indigo-400 rounded-lg outline-none focus:ring focus:ring-indigo-600 focus:ring-offset-2 focus:ring-opacity-50 transition ease-linear" href="${country.maps.googleMaps}" target="_blank">Map Of ${country.name.common}<span>
                 </div>
             `
+
             let toggleButton = document.querySelector('#toggle-btn');
             let flag = document.querySelector('#flag');
-            let coatOfArm = document.querySelector('#coat-of-arm');
+            let coatOfArms = document.querySelector('#coat-of-arms');
 
             toggleButton.addEventListener('click', () => {
-                if(coatOfArm.classList.contains('hidden')) {
+                if(coatOfArms.classList.contains('hidden')) {
                     flag.classList.add('hidden');
-                    coatOfArm.classList.remove('hidden');
+                    coatOfArms.classList.remove('hidden');
                     toggleButton.textContent = 'Flag';
                 }
                 else {
-                    coatOfArm.classList.add('hidden');
+                    coatOfArms.classList.add('hidden');
                     flag.classList.remove('hidden');
-                    toggleButton.textContent = 'Coat of Arm';
+                    toggleButton.textContent = 'Coat of Arms';
                 }
             })
         })
